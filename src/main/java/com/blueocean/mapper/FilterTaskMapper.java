367ca48dafaac77f1bf103380db3140ce8a718d7
jgj <jgj1101110@gmail.com>
2026-05-19 22:34:42 +0800

    2211

--- src/main/java/com/blueocean/mapper/FilterTaskMapper.java
+++
@@ -1,9 +0,0 @@
-package com.blueocean.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.blueocean.entity.FilterTask;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FilterTaskMapper extends BaseMapper<FilterTask> {
-}
